a195f190234b537b49e29ea24115de50e56c8c54
javier.duarte <reyaduarte@gmail.com>
2021-08-16 18:39:33 -0300

    Se agrego el template para el proyecto

--- QuizChacoApp/QuizChacoApp/urls.py
+++ QuizChacoApp/QuizChacoApp/urls.py
@@ -1,28 +1,14 @@
-"""QuizChacoApp URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
-from juego import views
+from core import views as core_views
+from juego import views as juego_views
 
 urlpatterns = [
-    path('', views.home, name = 'home'),
-    path('home/', views.home, name = 'home'),
-    path('acercade/', views.acercade, name = 'acercade'),
-    path('jugar/', views.jugar, name = 'jugar'),
-    path('ayuda/', views.ayuda, name = 'ayuda'),
-    path('miscelanea/', views.miscelanea, name = 'miscelanea'),
+    path('', core_views.inicio, name='inicio'),
+    path('inicio/', core_views.inicio, name='inicio'),
+    path('acercade/', core_views.acercade, name='acercade'),
+    path('juego/', juego_views.juego, name='juego'),
+    path('ayuda/', core_views.ayuda, name='ayuda'),
+    path('miscelanea/', core_views.miscelanea, name='miscelanea'),
     path('admin/', admin.site.urls)
 ]
--- QuizChacoApp/juego/views.py
+++ QuizChacoApp/juego/views.py
@@ -2,18 +2,8 @@ from django.shortcuts import render
 from core.models import Quiz
 
 # Create your views here.
-def home(request):
-    quiz = Quiz.objects.all()
-    return render(request, "juego/home.html", {'quiz': quiz})
 
-def acercade(request):
-    return render(request, "juego/acercade.html")
 
-def jugar(request):
-    return render(request, "juego/jugar.html")
-
-def ayuda(request):
-    return render(request,"juego/ayuda.html")
-
-def miscelanea(request):
-    return render(request,"juego/miscelanea.html")
+def juego(request):
+    quiz = Quiz.objects.order_by('-pk')[0]
+    return render(request, "juego/juego.html", {'quiz': quiz})
