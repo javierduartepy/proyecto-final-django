719a36a98039875aebe3f70c6163202b7ddb4f34
javier.duarte <reyaduarte@gmail.com>
2021-08-23 00:55:25 -0300

    Agrego la primera version del temporizador

--- juego/views.py
+++ juego/views.py
@@ -3,10 +3,12 @@ from core.models import Quiz
 
 # Create your views here.
 
+# mensaje:  incorrecto, casi casi, perfecto, lo tienes, tiempo fuera
 
-def juego(request):
+
+def juego(request, categoria, id):
     quiz = Quiz.objects.order_by('-pk')[0]
-    return render(request, "juego/juego.html", {'quiz': quiz})
+    return render(request, "juego/juego.html", {'quiz': quiz, 'categoria': categoria})
 
 
 def categoria(request):
