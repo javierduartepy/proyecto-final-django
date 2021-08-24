f53c4da8d58cb7fa766bfe235f53ec417387235a
javier.duarte <javierduarte81py@gmail.com>
2021-08-23 22:55:08 -0300

    Creación de tablas para acercade y sabias que

--- core/models.py
+++ core/models.py
@@ -2,10 +2,11 @@
 from django.db import models
 
 class Integrante(models.Model):
-    nombre = models.TextField()
+    nombre = models.CharField(max_length=100)
     rol = models.CharField(max_length=100)
     perfil = models.TextField()
     avatar = models.URLField(null = True, blank = True, verbose_name = 'Link de la imagen')
+    linkedin = models.URLField(null = True, blank = True, verbose_name = 'Link de Linkedin')
     created = models.DateTimeField(auto_now_add = True, verbose_name= 'Fecha de Creación')
     updated = models.DateTimeField(auto_now = True, verbose_name= 'Fecha de Edición')
     
@@ -21,7 +22,7 @@ class Miscelanea(models.Model):
     titulo = models.CharField(max_length=100)
     imagen = models.URLField(null = True, blank = True, verbose_name = 'Link de la imagen')
     informe = models.TextField()
-    link_informe = models.URLField(verbose_name = 'Link de la imagen')
+    link_informe = models.URLField(verbose_name = 'Link del informe')
     created = models.DateTimeField(auto_now_add = True, verbose_name= 'Fecha de Creación')
     updated = models.DateTimeField(auto_now = True, verbose_name= 'Fecha de Edición')
     
