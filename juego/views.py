2d3a82c7485351f034904c078ff1713107e544e1
javier.duarte <reyaduarte@gmail.com>
2021-08-28 14:08:50 -0300

    Agregue la validacion de preguntas

--- juego/views.py
+++ juego/views.py
@@ -11,7 +11,6 @@ def juego(request, categoriaId, preguntaId):
     pregunta = Pregunta.objects.get(pk=preguntaId)
     opciones = Opcion.objects.raw(
         f"SELECT * FROM opciones WHERE pregunta_id='{preguntaId}'")
-    # mensaje:  incorrecto, casi casi, perfecto, lo tienes, tiempo fuera
     return render(request, "juego/juego.html", {'pregunta': pregunta, 'categoria': categoria, 'opciones': opciones})
 
 
@@ -21,4 +20,14 @@ def categoria(request):
     if Pregunta.objects.filter(categoria_id=categoriaId).exists():
         preguntas = Pregunta.objects.filter(categoria_id=categoriaId).values()
     json_response = {'preguntas': list(preguntas)}
+    return JsonResponse(json_response)
+
+
+def opcionCorrecta(request):
+    opciones = None
+    preguntaId = request.GET.get('preguntaId', None)
+    if Opcion.objects.filter(pregunta_id=preguntaId).exists():
+        opciones = Opcion.objects.filter(pregunta_id=preguntaId).values()
+
+    json_response = {'opciones': list(opciones)}
     return JsonResponse(json_response)
