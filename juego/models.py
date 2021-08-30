15d9b7f819ad4b36e395f07a33b1e0e11cb1ec74
javier.duarte <javierduarte81py@gmail.com>
2021-08-30 01:35:06 -0300

    cambio imagen de fondo de los templates y cree la clase avatar

--- juego/models.py
+++ juego/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
+
 # Create your models here.
 
 
@@ -93,3 +95,6 @@ class Puntuacion(models.Model):
 
     class Meta:
         db_table = 'puntuaciones'
+    
+
+
